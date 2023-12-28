4c29c1a6b52fb172cfcfa6e7a78adf7dfd8e4696
mbosenisum <mbose@nisum.com>
2023-12-27 21:52:24 -0800

    packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,41 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyPlugin = require('copy-webpack-plugin');
 
+// https://webpack.js.org/concepts/entry-points/
 module.exports = {
-      module: {
-      rules: [
-        {
-          test: /\.html$/i,
-          loader: "html-loader",
-        },
-      ],
-    },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
   entry: {
-    index: './src/index.js',
+    // index: './src/index.js',
     // menu: './src/menu.js',
+    menu: './src/menu.js',
+    index: './src/index.js',
+    // index: {
+    //   dependOn: 'menu',
+    //   import: './src/index.js',
+    // },
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'placeholder title',
-      template: '/src/index.html'
+      template: '/src/index.html',
     }),
+    // new CopyPlugin([
+    //   { from: 'src', to: 'dist' },
+    // ]),
+    // new CopyPlugin({
+    //   patterns: [
+    //     { from: 'src/*.png', to: 'dist' },
+    //     __dirname,
+    //   ],
+    // }),
   ],
   output: {
     filename: '[name].js',
